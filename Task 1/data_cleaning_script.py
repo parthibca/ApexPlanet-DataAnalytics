82584744e48911ffc1cf467b63b75fce1f61234c
parthibca <144345858+parthibca@users.noreply.github.com>
2026-06-10 07:52:04 +0100

    Delete Task 1/data_cleaning_script.py

--- Task 1/data_cleaning_script.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-df=pd.read_excel('ApexPlanet_DataAnalytics_Dataset.xlsx')
-df['Order_Date']=pd.to_datetime(df['Order_Date'])
-df['Age']=df['Age'].fillna(df['Age'].median())
-df['City']=df['City'].fillna(df['City'].mode()[0])
-df.to_excel('Cleaned_ApexPlanet_Dataset.xlsx',index=False)
-print('Cleaning completed')
